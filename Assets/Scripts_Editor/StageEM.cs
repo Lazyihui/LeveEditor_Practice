0226634a11faa404f43a1a601ecfa7b49e0b8df9
lazyihui <chengbaozi6@qq.com>
2024-09-22 22:58:09 +0800

    <feature>add: StageEM

---
+++ Assets/Scripts_Editor/StageEM.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+
+
+public class StageEM : MonoBehaviour {
+
+    public int tyoeID;
+
+    public StageSo so;
+
+    // 第一件事： 将数据保存到TM里，
+    [ContextMenu("Save")]
+
+    public void Save() {
+    
+        so.tm.typeID = tyoeID;
+
+    }
+
+     
+
+}
